449dcf7a6b239e28ee1fb00c5e1d4612eec63219
Brixn <109771702+imbriankeeley@users.noreply.github.com>
2024-11-08 12:54:13 -0500

    spacing

--- src/List.jsx
+++ src/List.jsx
@@ -1,26 +1,27 @@
-import { ListItem } from "./ListItem"
+import { ListItem } from "./ListItem";
 
 export function List(props) {
-    if (!props.animals) {
-        return <div>Loading...</div>;
-    }
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
 
-    if (props.animals.length === 0) {
-        return <div>There are no animals in the list!</div>;
-    }
-    
-    return (
-        <ul>
-            {props.animals.map((animal) => {
-                // return <ListItem 
-                //         key={animal}
-                //         animal={animal}>
-                //         </ListItem>
-                // return animal.startsWith('L') ? 
-                // <li key = {animal}>{animal}</li> : null;
-                // return animal.startsWith('L') && <li key = {animal}>{animal}</li>
-                return <li key={animal}>{animal}</li>
-            })}
-        </ul>
-    );
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        // return <ListItem
+        //         key={animal}
+        //         animal={animal}>
+        //         </ListItem>
+        // return animal.startsWith('L') ?
+        // <li key = {animal}>{animal}</li> : null;
+        // return animal.startsWith('L') && <li key = {animal}>{animal}</li>
+        return <li key={animal}>{animal}</li>;
+      })}
+    </ul>
+  );
 }
+
